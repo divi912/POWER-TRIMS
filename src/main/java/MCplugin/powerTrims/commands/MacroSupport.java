382226d36d984bc0d10881d50df91a7376c1011c
Jamie <jamie.local@proton.me>
2025-10-13 06:06:35 -0600

    Create MacroSupport.java

---
+++ src/main/java/MCplugin/powerTrims/commands/MacroSupport.java
@@ -0,0 +1,52 @@
+// CommandKeys / Macro Support
+package MCplugin.powerTrims.commands;
+import MCplugin.powerTrims.Logic.AbilityManager;
+import org.bukkit.ChatColor;
+import java.util.HashMap;
+import MCplugin.powerTrims.Logic.ArmourChecking;
+import java.util.Map;
+import java.util.function.Consumer;
+import org.bukkit.inventory.meta.trim.TrimPattern;
+import org.bukkit.command.Command;
+import MCplugin.powerTrims.PowerTrimss;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class MacroSupport implements CommandExecutor {
+    private final PowerTrimss plugin;
+    private final AbilityManager abilityManager;
+    private final ArmourChecking armourChecking;
+    private final Map<TrimPattern, Consumer<Player>> primaryAbilities = new HashMap<>();
+
+    public MacroSupport(PowerTrimss plugin, AbilityManager abilityManager, ArmourChecking armourChecking) {
+        this.plugin = plugin;
+        this.abilityManager = abilityManager;
+        this.armourChecking = armourChecking;
+
+    }
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        
+        Player player = (Player) sender;
+        TrimPattern equippedTrim = armourChecking.getEquippedTrim(player);
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(ChatColor.RED + "This command can only be used by players.");
+            return true;
+        }
+        //if (equippedTrim != null && primaryAbilities.containsKey(equippedTrim)) {
+        if (equippedTrim == null) {
+            player.sendMessage(ChatColor.RED + "No Equipped Trim!");
+            
+        
+        } else {
+            abilityManager.activatePrimaryAbility(player);
+            player.sendMessage(ChatColor.RED + "Used Trim Power!");
+        }
+       
+        
+
+        return true;
+    }
+    
+}
